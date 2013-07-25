30df7d8457f878661adb6e911820c818164aa174
yulong <yulongdance@gmail.com>
2013-07-25 18:55:25 +0800

    Initial Commit

---
+++ Laifu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Laifu
+//
+//  Created by 于 龙 on 13-7-25.
+//  Copyright (c) 2013年 yulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Laifu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Laifu
+//
+//  Created by 于 龙 on 13-7-25.
+//  Copyright (c) 2013年 yulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
